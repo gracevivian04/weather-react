1f62aab11bda08681288ba9202d0df2f94b47bad
gracevivian04 <100720110+gracevivian04@users.noreply.github.com>
2022-08-13 14:24:09 +0100

    added axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(){
+    const [weatherData, setWeatherData] = useState({ready:false});
+    function handleResponse(response){
+        console.log(response.data);
+        setWeatherData({
+            ready: true,
+            temperature: response.data.main.temp,
+            wind: response.data.main.speed,
+            date: "Wednesday, 7:00",
+            description: response.data.main.description,
+            humidity: response.data.main.humidity,
+            city: response.data.name,
+        });
+        setTemperature(response.data.main.temp);
+    }
+
+    function search(){
+        const apiKey="cdec7dc86f02673d413e36697779fb47";
+        let city ="London";
+    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    }
+
+    if(weatherData.ready){
     return <div className="Weather">
         <form>
             <input type="search" 
@@ -13,23 +37,27 @@ export default function Weather(){
         <input type="submit" value="search" className="btn btn-primary w-100"/>
         </div>
         </form>
-        <h1>Lisbon</h1> 
+        <h1>{weatherData.city}</h1> 
         <ul>
-            <li> Wednesday 07:00</li>
-            <li> Mostly cloudy </li>
+            <li>{weatherData.date}</li>
+            <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row">
             <div className="col-6">
-                <span className="temperature"> 6</span>
+                <span className="temperature">{Math.round(weatherData.temperature)}</span>
                 <span className="unit"> °C </span>
             </div>
             <div className="col-6">
                 <ul>
                     <li>Precipitation: 15% </li>
                     <li>Humidity: 72% </li>
-                    <li>Wind: 13km/h </li>
+                    <li>Wind:{weatherData.wind}km/h </li>
                 </ul>
             </div>
         </div>
         </div>
-;}
+;} else {
+   search();
+   return "Loading..." 
+}
+}
