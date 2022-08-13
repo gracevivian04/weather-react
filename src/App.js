9a2950064d4e03434d386e23336d2bef24e65574
gracevivian04 <100720110+gracevivian04@users.noreply.github.com>
2022-08-13 13:31:43 +0100

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
     <div className="App"> 
     <div className="container">
     <h1> Weather App </h1>
+    <Weather />
     <footer>
       This project is coded by Grace Vivian and is 
     <a href="https://github.com/gracevivian04/weather-react"
